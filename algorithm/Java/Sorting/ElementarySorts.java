2432a5bbb4a42f6364c4f329ca0c108aa4fa8636
BobWong <wangbo@bluemoon.com.cn>
2017-07-25 15:46:55 +0800

    Modify

---
+++ algorithm/Java/Sorting/ElementarySorts.java
@@ -0,0 +1,8 @@
+/**
+ * Elementary Sorts Entry Class
+ */
+public class ElementarySorts {
+    public static void main(String[] args) {
+        System.out.println("122hello sorts  new string");
+    }
+}
