893ed84bfec5127c21b7f0736bc065054d1b787d
BobWong <wangbo@bluemoon.com.cn>
2017-07-27 13:28:30 +0800

    Modify

--- algorithm/Java/Sorting/ElementarySorts/ElementarySorts.java
+++ algorithm/Java/Sorting/ElementarySorts/ElementarySorts.java
@@ -8,6 +8,27 @@ public class ElementarySorts
         Student s1 = new Student(25);
 
         System.out.println("s0 compare to s1: " + s0.compareTo(s1));
+
+        
+    }
+
+    public static boolean isSorted(Comparable<Comparable>[] a) {   
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i].compareTo(a[i + 1]) > 0) return false;  // 增序
+        }
+        return true;
+    }
+
+    public static void exchange(Comparable[] a, int i, int j) {
+        Comparable tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    public static void showArray(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("array[" + i + "]" + "=" + a[i]);
+        }
     }
 }
 
