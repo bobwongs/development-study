f20b1affb0665502152e6ae61c7dfb4291e6878c
BobWong <wangbo@bluemoon.com.cn>
2017-08-02 14:32:37 +0800

    Modify

--- design-pattern/foundamentals/Foundamentals.java
+++ design-pattern/foundamentals/Foundamentals.java
@@ -5,41 +5,103 @@
 public class Foundamentals 
 {
     public static void main(String[] args) {
-        System.out.print("hi");
+        Duck duck = new Duck();
+        duck.quack();
+        duck.fly();
+        duck.flyable = new FlyHighly();
+        duck.fly();
+        duck.flyable = new FlyLowly();
+        duck.fly();
+
+        System.out.println("duck age: " + duck.getAge());
+        duck.setAge(5);
+        System.out.println("duck age: " + duck.getAge());
+
+        YellowDuck yellowDuck = new YellowDuck();
+        yellowDuck.showYellow();
     }
 }
 
 /**
- * Duck
+ * Duck, Encapsulation
  */
 class Duck 
 {
-    Flyable flyable;
+    Flyable flyable;  // Polymorphism
+    private int age;
 
-    public Duck () { }
+    public Duck () {
+        this.setAge(1);
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
 
     public void quack() {
         System.out.println("Quack");
     }
 
     public void fly() {
-        this.flyable.fly();
+        if (this.flyable != null) {
+            this.flyable.fly();
+        } else {
+            System.out.println("I'm can not flyable");
+        }
     }
 }
 
 /**
- * YellowDuck
+ * YellowDuck, Inheritance
  */
 class YellowDuck extends Duck
 {
     public void showYellow() {
-        System.out.println("I'm in yellow color");
+        System.out.println("I'm duck in yellow color");
     }
 }
 
 /**
  * Flyable
  */
-interface Flyable {
+interface Flyable 
+{
     public void fly();
+}
+
+/**
+ * FlyHighly
+ */
+class FlyHighly implements Flyable
+{
+    public FlyHighly () { }
+
+    // Polymorphism
+    @Override
+    public void fly() {
+        System.out.println("I can fly highly!");
+    }
+}
+
+/**
+ * FlyLowly
+ */
+class FlyLowly implements Flyable
+{
+    public FlyLowly () { }
+
+    @Override
+    public void fly() {
+        System.out.println("I can fly lowly!");
+    }
 }
