58d814f7221a39de5c8dbe46bbd32d7c065a05a2
BobWong <wangbo@bluemoon.com.cn>
2018-07-17 15:45:49 +0800

    Modify

--- Web/vue/demo1/src/components/ToDoList.vue
+++ Web/vue/demo1/src/components/ToDoList.vue
@@ -2,56 +2,68 @@
   <div class="container">
     <div class="header">To Do List</div>
     <div class="middle">
-      <input class="input-search" placeholder="To Add Title" />
+      <input class="input-search" placeholder="To Add Title" v-model="toAddTitle" v-on:keyup.enter="add" />
       <router-link to="/to-do-item">
         <button class="button-add">Add</button>
       </router-link>
     </div>
     <div class="to-do-list">
-      <to-do-item v-for="(item, index) in items" :key="index" :title="item.title" :content="item.content" :finished="item.finished" />
+      <div class="list-item" v-for="(item, index) in items" :key="index">
+        <div class="list-item-title">{{item.title}}</div>
+        <div class="list-item-content">{{item.content}}</div>
+        <div class="list-item-finished">{{item.finished}}</div>
+      </div>
+      <h1>{{testTitle}}</h1>
+      <MyView :titleToChange="testTitle" v-on:tapAction="tapAction" />
     </div>
+    <button class="test-button" v-on:click="test">Test</button>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-Vue.component('to-do-item', {
-  props: {
-    title: String,
-    content: String,
-    finished: Boolean
-  },
-  data: function () {
-    return {
-      
-    }
-  },
-  template: `
-    <div>
-      <div class="item-title" style="background: orange;">{{title}}</div>
-      <div class="item-content">{{content}}</div>
-      <div class="item-finished">{{finished}}</div>
-    </div>
-  `,
-  style: `
-    .item-title {
-      background: orange;
-    }
-  `
-})
+import MyView from './MyView.vue'
 
+let initialItems = [
+  {
+    title: 'go home',
+    content: 'as quickly as possible',
+    finished: false
+  },
+  {
+    title: 'go to work',
+    content: 'to earn money',
+    finished: false
+  }
+]
+var testTitle = 1
 export default {
   name: 'ToDoList',
+  components: {
+    MyView
+  },
   data () {
     return {
-      items: [
-        {
-          title: 'go home',
-          content: 'as quickly as possible',
-          finished: false
-        }
-      ]
-    } 
+      'items': initialItems,
+      'toAddTitle': 'Initial To Add Title',
+      testTitle
+    }
+  },
+  methods: {
+    test: function (event) {
+      console.log('test')
+      this.testTitle += 1
+    },
+    add: function (event) {
+      console.log('add')
+      let todo = {title: this.toAddTitle, content: 'new added', finished: false}
+      this.items = [...this.items, todo]
+      console.log(this.items)
+
+      this.toAddTitle = ''
+    },
+    tapAction: function (event) {
+      console.log('tap act')
+    }
   }
 }
 </script>
@@ -75,17 +87,17 @@ export default {
 .middle {
   padding: 0 12px 0 12px;
   height: 60px;
-  background-color: black;
-  opacity: 0.1;
+  background-color: rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: center;
 }
 
 .input-search {
- flex-grow: 1;
- height: 30px;
- color: black;
- font-size: 12px;
+  flex-grow: 1;
+  height: 30px;
+  color: black;
+  font-size: 12px;
+  background: darkkhaki;
 }
 
 .button-add {
@@ -101,6 +113,20 @@ export default {
   right: 0;
   top: 124px;
   bottom: 0;
+}
+
+.list-item {
+  margin-bottom: 12px;
+  background: lightgray;
+}
+
+.test-button {
+  background: #996633;
+  position: fixed;
+  left: 0;
+  width: 100%;
+  bottom: 0;
+  height: 50px;
 }
 
 </style>
